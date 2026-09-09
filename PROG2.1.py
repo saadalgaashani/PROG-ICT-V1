cb2d7deba12de39d8d359531c98b74fea20f7e75
saadalgaashani <saad.algaashani@gmail.com>
2026-09-09 20:38:46 +0200

    2.1

---
+++ PROG2.1.py
@@ -0,0 +1,6 @@
+cijferPROJA = 10
+cijferPROG = 10
+cijferMOD = 10
+gemidled = (cijferPROJA + cijferPROG + cijferMOD) / 3
+belooning =  (cijferPROJA + cijferPROG + cijferMOD) *30
+print ("het gemidled cijfer in dit blok is :", gemidled ," en het totaal belooning is:",belooning )
