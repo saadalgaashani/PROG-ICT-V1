9c8eee1eeec0e1b10d1403887d587d244f3dc8ce
saadalgaashani <saad.algaashani@gmail.com>
2026-09-20 15:09:30 +0200

    weerstaion extra punten

--- Weerstation.py
+++ Weerstation.py
@@ -26,45 +26,49 @@ def weerrapport(temp_celcius, windsnelheid, luchtvochtigheid):
 
 def weerstation():
     geregsteerd_temp = []
+    geregsteerd_datum = []
     for dag in range(1,8):
+        datum = input("Wat is de datum(daf-maand-jaar):")
+        if datum == "":
+            print("\033[91m"+"bye"+"\033[0m")
+            break
+        else:
+            geregsteerd_datum.append(datum)
+
         temp_invoer= input(f"Wat is op dag {dag}  de temperatuur[C]:")
         if temp_invoer =="":
-            print("bye")
+            print("\033[91m"+"bye"+"\033[0m")
             break
         else:
             temp_invoer = float(temp_invoer)
 
         wind_invoer= input(f"Wat is op dag {dag} de windsnelheid[m/s]:")
         if wind_invoer == "":
-            print("bye")
+            print("\033[91m"+"bye"+"\033[0m")
             break
         else:
             wind_invoer = float(wind_invoer)
 
         vocht_invoer= input(f"Wat is op dag {dag} de vochtigheid[%]:")
         if vocht_invoer == "":
-            print("bye")
+            print("\033[91m"+"bye"+"\033[0m")
             break
         else:
             vocht_invoer = float(vocht_invoer)
 
+
+
         geregsteerd_temp.append(float(temp_invoer))
         gimedeld = sum(geregsteerd_temp) / len(geregsteerd_temp)
-        print("Het is",str(temp_invoer)+"C","("+ str(fahrenheit(temp_invoer))+"F"+")")
-        print(weerrapport(temp_invoer,wind_invoer,vocht_invoer))
-        print("Gem.temp tot nu toe is",round(gimedeld,1))
+        print("\033[94m"+"Het is",str(temp_invoer)+"C","("+ str(fahrenheit(temp_invoer))+"F"+")"+"\033[0m")
+        print("\033[94m"+str(weerrapport(temp_invoer,wind_invoer,vocht_invoer))+"\033[0m")
+        print("\033[94m"+"Gem.temp tot nu toe is"+"\033[0m",round(gimedeld,1))
         print("="*39)
 
-
-
-
-
-
-
-
-
-
-
+    if len(geregsteerd_datum) >= 1:
+        print("\033[93m"+"hier zijn al gereegsteerd datum(s)", "\n", str(geregsteerd_datum)+"\033[0m")
+    else:
+        print("\033[91m"+"Sorry, er is geen gereegsteerd datum(s) gevonden"+"\033[0m")
 
 
 weerstation()
